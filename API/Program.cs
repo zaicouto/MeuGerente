249f509d07ace1e5d197b02d62825f8f14e90a5e
Ozair Couto <marloncouto719@gmail.com>
2025-07-19 18:31:37 -0300

    Adiciona configuração de banco e validação fluente
    Inclui namespaces necessários, configura o contexto do banco de dados `OrdersDbContext` para MySQL, registra o repositório de pedidos e configura o MediatR. Adiciona validação automática e adaptadores do lado do cliente para FluentValidation.

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,34 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using Modules.Orders.Application.Commands;
+using Modules.Orders.Application.Validators;
+using Modules.Orders.Domain.Interfaces;
+using Modules.Orders.Infrastructure.Persistence;
+using Modules.Orders.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<OrdersDbContext>(options =>
+    options.UseMySql(
+        // TODO: usar variável de ambiente para a string de conexão
+        builder.Configuration.GetConnectionString("OrdersDb"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("OrdersDb"))
+    )
+);
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(CreateOrderCommandHandler).Assembly)
+);
+
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
 
 var app = builder.Build();
 
