d0a8c38284130908f8be8f8a63d1de936bb1639c
Ozair Couto <marloncouto719@gmail.com>
2025-07-21 19:47:15 -0300

    Validação do comando de criar pedido

--- Modules.Orders/Application/Validators/CreateOrderCommandValidator.cs
+++ Modules.Orders/Application/Validators/CreateOrderCommandValidator.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using Modules.Orders.Application.Commands;
 
-namespace Modules.Orders.Application.Validators
+namespace Modules.Orders.Application.Validators;
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
 {
-    internal class CreateOrderCommandValidator
+    public CreateOrderCommandValidator()
     {
+        RuleFor(c => c.Items).NotEmpty().WithMessage("At least one item is required.");
+
+        RuleForEach(c => c.Items)
+            .ChildRules(items =>
+            {
+                items
+                    .RuleFor(i => i.ProductName)
+                    .NotEmpty()
+                    .WithMessage("Product name is required.");
+
+                items
+                    .RuleFor(i => i.Quantity)
+                    .GreaterThan(0)
+                    .WithMessage("Quantity must be greater than zero.");
+
+                items
+                    .RuleFor(i => i.UnitPrice)
+                    .GreaterThan(0)
+                    .WithMessage("Unit price must be greater than zero.");
+            });
     }
 }
