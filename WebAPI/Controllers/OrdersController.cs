0020a45e3ba68895deb696c875ff5f5426fc0f07
Ozair Couto <marloncouto719@gmail.com>
2025-07-21 19:53:07 -0300

    Controlador das requisições de pedidos

---
+++ WebAPI/Controllers/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Modules.Orders.Application.Commands;
+using Modules.Orders.Application.Queries;
+using Modules.Orders.Infrastructure.Persistence;
+using Modules.Orders.Infrastructure.Persistence.Seed;
+using WebAPI.Infrastructure.Filters;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("api/orders")]
+public class OrdersController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateOrderCommand command)
+    {
+        var id = await mediator.Send(command);
+        return CreatedAtAction(nameof(GetById), new { id }, id);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var order = await mediator.Send(new GetOrderByIdQuery(id));
+        return order == null ? NotFound() : Ok(order);
+    }
+
+    [HttpPost("reset-db")]
+    [DevOnly]
+    public async Task<IActionResult> ResetDb([FromServices] OrdersDbContext context)
+    {
+        await OrdersDbSeeder.TruncateAsync(context);
+        await OrdersDbSeeder.SeedAsync(context);
+        return Ok("Reset completed!");
+    }
+}
