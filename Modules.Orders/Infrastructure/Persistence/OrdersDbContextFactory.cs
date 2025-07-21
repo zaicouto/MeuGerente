1458f7d025f3ae20364911b994570516e3c1cdba
Ozair Couto <marloncouto719@gmail.com>
2025-07-21 19:50:12 -0300

    Contexto do DB para execução de CLI

---
+++ Modules.Orders/Infrastructure/Persistence/OrdersDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Shared.Helpers;
+
+namespace Modules.Orders.Infrastructure.Persistence;
+
+public class OrdersDbContextFactory : IDesignTimeDbContextFactory<OrdersDbContext>
+{
+    public OrdersDbContext CreateDbContext(string[] args)
+    {
+        // Get the appsettings from the root of the API project
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(PathHelpers.GetSolutionRootPath(), "WebAPI"))
+            .AddJsonFile("appsettings.Development.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("OrdersDb");
+
+        var optionsBuilder = new DbContextOptionsBuilder<OrdersDbContext>();
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+        return new OrdersDbContext(optionsBuilder.Options);
+    }
+}
