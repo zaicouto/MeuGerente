beaa045c9bd3f9158f9a0a37a8822794f1d08bdd
Ozair Couto <marloncouto719@gmail.com>
2025-07-21 19:27:42 -0300

    Adiciona arquivo para definir o comportamento de validação

---
+++ Modules.Orders/Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Modules.Orders.Application.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken
+    )
+    {
+        var context = new ValidationContext<TRequest>(request);
+
+        var failures = validators
+            .Select(v => v.Validate(context))
+            .SelectMany(r => r.Errors)
+            .Where(f => f != null)
+            .ToList();
+
+        if (failures.Count > 0)
+            throw new ValidationException(failures);
+
+        return await next(CancellationToken.None);
+    }
+}
