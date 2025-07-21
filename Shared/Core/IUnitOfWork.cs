b7c0c5f9304a44f813cc3505d4fcd5e164d09082
Ozair Couto <marloncouto719@gmail.com>
2025-07-21 19:52:27 -0300

    Contrato para as unidades de trabalho

--- Shared/Core/IUnitOfWork.cs
+++ Shared/Core/IUnitOfWork.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Shared.Core;
 
-namespace Shared.Core
+public interface IUnitOfWork
 {
-    internal interface IUnitOfWork
-    {
-    }
+    Task<int> SaveChangesAsync();
 }
