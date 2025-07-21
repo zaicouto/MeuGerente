212f69e4fbb22b35c0192f611d5d3ae8c28d98a9
Ozair Couto <marloncouto719@gmail.com>
2025-07-21 19:51:01 -0300

    Repositório para tabelas de pedidos

--- Modules.Orders/Infrastructure/Repositories/OrderRepository.cs
+++ Modules.Orders/Infrastructure/Repositories/OrderRepository.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Modules.Orders.Domain.Entities;
+using Modules.Orders.Domain.Interfaces;
+using Modules.Orders.Infrastructure.Persistence;
 
-namespace Modules.Orders.Infrastructure.Repositories
+namespace Modules.Orders.Infrastructure.Repositories;
+
+public class OrderRepository(OrdersDbContext dbContext) : IOrderRepository
 {
-    internal class OrderRepository
-    {
-    }
+    public async Task<Order?> GetByIdAsync(Guid id) =>
+        await dbContext.Orders.Include(o => o.Items).FirstOrDefaultAsync(o => o.Id == id);
+
+    public async Task AddAsync(Order order) => await dbContext.Orders.AddAsync(order);
+
+    public async Task SaveChangesAsync() => await dbContext.SaveChangesAsync();
 }
