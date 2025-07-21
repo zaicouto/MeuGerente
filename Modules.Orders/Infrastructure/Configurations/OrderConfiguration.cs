f445f06aaf9d3baeb52de90bc7abb8347368c966
Ozair Couto <marloncouto719@gmail.com>
2025-07-21 19:48:43 -0300

    Configuração da tabela Orders no EF

--- Modules.Orders/Infrastructure/Configurations/OrderConfiguration.cs
+++ Modules.Orders/Infrastructure/Configurations/OrderConfiguration.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Modules.Orders.Domain.Entities;
 
-namespace Modules.Orders.Infrastructure.Configurations
+namespace Modules.Orders.Infrastructure.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
 {
-    internal class OrderConfiguration
+    public void Configure(EntityTypeBuilder<Order> builder)
     {
+        builder.ToTable("Orders");
+
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.CreatedAt).IsRequired();
+
+        builder.Property(o => o.Status).IsRequired();
+
+        builder
+            .HasMany(o => o.Items)
+            .WithOne()
+            .HasForeignKey(oi => oi.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
