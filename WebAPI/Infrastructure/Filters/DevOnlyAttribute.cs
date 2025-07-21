80f206c9cbe6359e13dcf0a437516d60223d54e4
Ozair Couto <marloncouto719@gmail.com>
2025-07-21 19:53:43 -0300

    Filtro para endpoints exclusivos para desenvolvimento

---
+++ WebAPI/Infrastructure/Filters/DevOnlyAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebAPI.Infrastructure.Filters;
+
+public class DevOnlyAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var env = context.HttpContext.RequestServices.GetService<IWebHostEnvironment>();
+
+        if (env != null && !env.IsDevelopment() && env.EnvironmentName != "Testing")
+        {
+            context.Result = new NotFoundResult();
+        }
+    }
+}
