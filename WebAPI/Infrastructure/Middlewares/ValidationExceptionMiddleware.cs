3dadd9eaa61af9c772efad8fb0c30b3079c6d855
Ozair Couto <marloncouto719@gmail.com>
2025-07-21 19:54:07 -0300

    Middleware para interceptar os erros de validação e exibir uma mensagem formatada

---
+++ WebAPI/Infrastructure/Middlewares/ValidationExceptionMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace WebAPI.Infrastructure.Middlewares;
+
+public class ValidationExceptionMiddleware(RequestDelegate next)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (FluentValidation.ValidationException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Response.ContentType = "application/json";
+
+            var errors = ex
+                .Errors.GroupBy(e => e.PropertyName)
+                .ToDictionary(g => g.Key, g => g.Select(e => e.ErrorMessage).ToArray());
+
+            var response = new { Errors = errors };
+            await context.Response.WriteAsJsonAsync(response);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception: " + ex.InnerException);
+        }
+    }
+}
