1b70a66a0c52a7d105c16da7a62a4467fe23e066
Ozair Couto <marloncouto719@gmail.com>
2025-07-24 19:12:59 -0300

    Adiciona anotações para o Swagger

--- WebAPI/Controllers/OrdersController.cs
+++ WebAPI/Controllers/OrdersController.cs
@@ -1,33 +1,70 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Modules.Orders.Application.Commands;
+using Modules.Orders.Application.DTOs;
 using Modules.Orders.Application.Queries;
 using Modules.Orders.Infrastructure.Persistence;
 using Modules.Orders.Infrastructure.Persistence.Seed;
+using Swashbuckle.AspNetCore.Annotations;
 using WebAPI.Infrastructure.Filters;
 
 namespace WebAPI.Controllers;
 
 [ApiController]
 [Route("api/orders")]
+[Consumes("application/json")]
+[Produces("application/json")]
 public class OrdersController(IMediator mediator) : ControllerBase
 {
+    /// <summary>
+    /// Cria um novo pedido.
+    /// </summary>
+    /// <param name="command"></param>
+    /// <returns></returns>
     [HttpPost]
+    [ProducesResponseType(typeof(string), StatusCodes.Status201Created)]
+    [SwaggerOperation(
+        Summary = "Cria um novo pedido.",
+        Description = "Cria um novo pedido com os dados informados."
+    )]
+    [SwaggerResponse(StatusCodes.Status201Created, "Pedido criado com sucesso.")]
     public async Task<IActionResult> Create(CreateOrderCommand command)
     {
         var id = await mediator.Send(command);
         return CreatedAtAction(nameof(GetById), new { id }, id);
     }
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetById(string id)
+    /// <summary>
+    /// Busca um pedido pelo ID.
+    /// </summary>
+    /// <param name="orderId">ID do pedido.</param>
+    /// <returns>foo</returns>
+    [HttpGet("{orderId}")]
+    [ProducesResponseType(typeof(OrderDto), StatusCodes.Status200OK)]
+    [SwaggerOperation(
+        Summary = "Busca um pedido pelo ID.",
+        Description = "Exibe os detalhes de um pedido."
+    )]
+    [SwaggerResponse(StatusCodes.Status201Created, "Pedido encontrado.")]
+    public async Task<IActionResult> GetById(string orderId)
     {
-        var order = await mediator.Send(new GetOrderByIdQuery(id));
+        var order = await mediator.Send(new GetOrderByIdQuery(orderId));
         return order == null ? NotFound() : Ok(order);
     }
 
+    /// <summary>
+    /// [Dev Only] Redefine a collection de pedidos.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
     [HttpPost("reset-db")]
     [DevOnly]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [SwaggerOperation(
+        Summary = "[Dev Only] Redefine a collection de pedidos.",
+        Description = "Apaga todos os documentos e depois popula a collection com dados de teste."
+    )]
+    [SwaggerResponse(StatusCodes.Status201Created, "Reset da collection finalizado.")]
     public async Task<IActionResult> ResetDb([FromServices] OrdersDbContext context)
     {
         await OrdersDbSeeder.TruncateAsync(context);
